3a4ac8a289e1dcbf89314eb6b392ea329997c13d
Dedev <mmachadoacm@gmail.com>
2023-02-03 00:38:03 -0300

    ajustando packages

---
+++ src/Modelo/SaldoInsuficienteException.java
@@ -0,0 +1,8 @@
+package Modelo;
+public class SaldoInsuficienteException extends Exception{
+	
+	public SaldoInsuficienteException(String msg) {
+		super(msg);
+	}
+
+}
---
+++ src/Modelo/SeguroDeVida.java
@@ -0,0 +1,9 @@
+package Modelo;
+public class SeguroDeVida implements Tributavel {
+
+	@Override
+	public double getValorImposto() {
+		return 42;
+	}
+
+}
